01e7636fcd9515ce533e4aff7c2903d6c9e484b2
Pierre <iampierreandre@gmail.com>
2026-04-17 11:18:20 -0700

    Use standalone output for Cloudflare Workers/OpenNext compatibility

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  images: { unoptimized: true },
+  output: 'standalone',
 };
 module.exports = nextConfig;
